21c19204ea29859a5823ae58d7a98d50d94efb4d
Md. Zahirul Islam <zahirul.ca24@gmail.com>
2026-08-08 03:50:03 +0600

    feat: register engagement router and models

--- app/main.py
+++ app/main.py
@@ -12,11 +12,13 @@ from app.foundation.http import ApiError, CorrelationIdMiddleware, correlation_i
 from app.routers.admin import router as admin_router
 from app.routers.auth import router as auth_router
 from app.routers.clients import router as clients_router
+from app.routers.engagements import router as engagements_router
 from app.routers.manager import router as manager_router
 from app.routers.staff import router as staff_router
 from app.routers.workforce import router as workforce_router
 import app.client_lifecycle_models  # noqa: F401
 import app.client_models  # noqa: F401
+import app.engagement_models  # noqa: F401
 import app.staff_models  # noqa: F401
 import app.workforce_models  # noqa: F401
 import app.workflow_models  # noqa: F401
@@ -75,6 +77,7 @@ app.include_router(manager_router)
 app.include_router(clients_router)
 app.include_router(staff_router)
 app.include_router(workforce_router)
+app.include_router(engagements_router)
 
 
 @app.get("/health")
